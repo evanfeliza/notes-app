11c2b4a21a4abf5fb814588fcb586109b31b42cb
evanfeliza <felizavndrd567@gmail.com>
2023-04-29 13:53:56 +0800

    refactor:  appId environment variable renamed

--- notes-application/firebase.ts
+++ notes-application/firebase.ts
@@ -9,7 +9,7 @@ const firebaseConfig = {
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET as string,
   messagingSenderId: process.env
     .NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID as string,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID as string,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_APP_ID as string,
   measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID as string,
 };
 
