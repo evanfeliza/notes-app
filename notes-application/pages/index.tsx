e054ccdeca3814fedb4118637a8a9280f43f2861
evanfeliza <felizavndrd567@gmail.com>
2023-04-29 13:28:11 +0800

    bugfix: user auto redirect

--- notes-application/pages/index.tsx
+++ notes-application/pages/index.tsx
@@ -1,16 +1,28 @@
-import React, { useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Login from "../components/Authentication/Login";
 import SignUp from "@/components/Authentication/SignUp";
 import { CgNotes } from "react-icons/cg";
 import Head from "next/head";
+import { AuthContext } from "@/context/auth/AuthContext";
+import { useRouter } from "next/router";
 
 export default function Home() {
   const [isRegistered, setIsRegistered] = useState(false);
+  const router = useRouter();
+  const { currentUser } = useContext(AuthContext);
+
+  useEffect(() => {
+    if (currentUser) {
+      router.push(`/dashboard/${currentUser.uid}`);
+    }
+  }, [currentUser, router]);
+
   return (
     <React.Fragment>
       <Head>
         <title key="title">NotesApp&apos;s Home</title>
       </Head>
+
       <main className="min-h-[100vh] w-full flex justify-center items-center p-4">
         <div className="max-h-full w-full xs:4/5 sm:w-2/3 lg:w-1/2 sm:grid grid-cols-2 shadow-md rounded-lg overflow-hidden">
           <div className="flex items-center justify-center bg-cyan-400 px-2 py-4">
