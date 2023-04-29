4951acdd103c6b53fc3445d3b8a68ea5a922c966
evanfeliza <felizavndrd567@gmail.com>
2023-04-29 13:25:37 +0800

    hotfix: mutation input fixed

--- notes-application/components/Dash/Navbar.tsx
+++ notes-application/components/Dash/Navbar.tsx
@@ -1,47 +1,43 @@
-import { signOut } from 'firebase/auth';
-import React, { useState,useContext } from 'react'
+import { signOut } from "firebase/auth";
+import React, { useState, useContext } from "react";
 import { FaUserCircle } from "react-icons/fa";
-import { auth } from '../../firebase'
-import { AuthContext } from '@/context/auth/AuthContext';
-import { useMutation } from 'urql';
-import { DELETE_USER } from '../GraphQl/Mutation';
+import { auth } from "../../firebase";
+import { AuthContext } from "@/context/auth/AuthContext";
+import { useMutation } from "urql";
+import { DELETE_USER } from "../GraphQl/Mutation";
 
 interface UserProp {
-  id: string,
-  name: string
+  id: string;
+  name: string;
 }
 
 interface User {
-  user?: UserProp
+  user?: UserProp;
 }
 
 interface NavProp {
-  data: User
+  data: User;
 }
 
-
-
-
 const Navbar: React.FC<NavProp> = ({ data }) => {
-  const { currentUser } = useContext(AuthContext)
-  const [isSettings, setIsSettings] = useState(false)
-  const [deleteUserResult, deleteUserMutation] = useMutation(DELETE_USER)
+  const { currentUser } = useContext(AuthContext);
+  const [isSettings, setIsSettings] = useState(false);
+  const [deleteUserResult, deleteUserMutation] = useMutation(DELETE_USER);
 
-  const { fetching } = deleteUserResult
- 
+  const { fetching } = deleteUserResult;
+
   const handleSignOut = () => {
     signOut(auth); // also sign out from Firebase
   };
 
   const handleDeleteUser = async () => {
-    try{
+    try {
       await auth.currentUser?.delete();
-      await deleteUserMutation({ id: currentUser.uid as string });
-    }catch(error){
-      console.log(error)
+      await deleteUserMutation({ id: data.user?.id as string });
+    } catch (error) {
+      console.log(error);
     }
   };
-
 
   return (
     <nav className="bg-cyan-500 w-full">
@@ -49,26 +45,44 @@ const Navbar: React.FC<NavProp> = ({ data }) => {
         <div className="flex items-center justify-between h-16 p-2">
           <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
             <div className="flex-shrink-0">
-              <h3 className="text-white text-2xl md:text-4xl font-extrabold uppercase ">Notes App</h3>
+              <h3 className="text-white text-2xl md:text-4xl font-extrabold uppercase ">
+                Notes App
+              </h3>
             </div>
           </div>
           <div className="md:flex items-center justify-center rounded-full">
-            <h1 className="hidden md:flex text-white text-lg font-semibold">{data && data.user && data.user.name}</h1>
-            <button className='relative text-black p-2' onClick={() => setIsSettings(prevIsSetting => !prevIsSetting)}>
-              <span className='text-white text-3xl md:text-4xl hover:border hover:boreder-1 hover:border-white hover:text-slate-400 duration-500  active:text-cyan-300'>
+            <h1 className="hidden md:flex text-white text-lg font-semibold">
+              {data && data.user && data.user.name}
+            </h1>
+            <button
+              className="relative text-black p-2"
+              onClick={() => setIsSettings((prevIsSetting) => !prevIsSetting)}
+            >
+              <span className="text-white text-3xl md:text-4xl hover:border hover:boreder-1 hover:border-white hover:text-slate-400 duration-500  active:text-cyan-300">
                 <FaUserCircle />
               </span>
             </button>
-            {isSettings &&
-              <ul className='z-50 absolute top-[60px] right-5 sm:right-[50px] lg:right-[200px] bg-white rounded-lg drop-shadow-md overflow-hidden divide-y divide-y-1 divide-cyan-500'>
-                <li className='cursor-pointer px-4 py-1 hover:bg-slate-300 active:bg-cyan-500 ' onClick={handleSignOut}>Sign Out</li>
-                <li className='cursor-pointer px-4 py-1 hover:bg-slate-300 active:bg-cyan-500' onClick={handleDeleteUser}>Delete User</li>
-              </ul>}
+            {isSettings && (
+              <ul className="z-50 absolute top-[60px] right-5 sm:right-[50px] lg:right-[200px] bg-white rounded-lg drop-shadow-md overflow-hidden divide-y divide-y-1 divide-cyan-500">
+                <li
+                  className="cursor-pointer px-4 py-1 hover:bg-slate-300 active:bg-cyan-500 "
+                  onClick={handleSignOut}
+                >
+                  Sign Out
+                </li>
+                <li
+                  className="cursor-pointer px-4 py-1 hover:bg-slate-300 active:bg-cyan-500"
+                  onClick={handleDeleteUser}
+                >
+                  Delete User
+                </li>
+              </ul>
+            )}
           </div>
         </div>
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
